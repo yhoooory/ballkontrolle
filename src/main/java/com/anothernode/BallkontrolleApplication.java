4dd58820de97e15c4d714fbb1c949679c43e7342
Moritz Reiter <moritz@anothernode.com>
2015-12-18 16:29:33 +0100

    Initial commit of gradle project

---
+++ src/main/java/com/anothernode/BallkontrolleApplication.java
@@ -0,0 +1,12 @@
+package com.anothernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallkontrolleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallkontrolleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/anothernode/BallkontrolleApplicationTests.java
@@ -0,0 +1,18 @@
+package com.anothernode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BallkontrolleApplication.class)
+@WebAppConfiguration
+public class BallkontrolleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
