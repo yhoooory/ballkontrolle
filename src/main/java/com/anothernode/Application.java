b4958cc117460bb5fcbbb8c250d83d2f030c20a2
Moritz Reiter <moritz@anothernode.com>
2015-12-22 16:43:54 +0100

    Rename application class

--- src/main/java/com/anothernode/Application.java
+++ src/main/java/com/anothernode/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BallkontrolleApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BallkontrolleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
